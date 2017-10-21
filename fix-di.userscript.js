9b045a5a7e8034e07fc301cc725522b661591e00
Mikael Levén <mikaelleven@users.noreply.github.com>
2017-10-21 17:40:56 +0200

    Added downloadUrl and bumped version

--- fix-di.userscript.js
+++ fix-di.userscript.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         DI++
-// @version      0.1
-// @description  Unlocks Di Plus articles and amends som visual issues
+// @version      0.2
+// @description  Unlocks DI Plus articles and amends som visual issues
 // @description  >> Download Tampermonkey to your favourite browser to enable this script: https://tampermonkey.net/ <<
 // @author       Mikael Levén
 // @namespace    http://labs.leven.se/
-// @downloadURL  https://raw.githubusercontent.com/mikaelleven/
+// @downloadURL  https://raw.githubusercontent.com/mikaelleven/userscripts-di-fix/master/fix-di.userscript.js
 // @match        https://www.di.se/*
 // @grant        GM_addStyle
 // ==/UserScript==
@@ -23,9 +23,5 @@
 
 	// Remove empty elements when using a ad-blocker
 	GM_addStyle ('.ad_container { display:none!important; }');
-
-	//GM_addStyle ('.di_header__scale js_di_header__scale { display:none!important; }');
-	//GM_addStyle ('.di_header__fixed-element js_header { display:none!important; }');
-
 
 })();
